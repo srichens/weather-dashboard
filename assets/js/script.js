e2164bbe4ce9d893dc7edafb231646a347faeea9
Sarah Richens <sarahgrichens@gmail.com>
2023-01-19 14:39:10 -0600

    event listener for search button, cities saved to storage and then retrieved from storage as buttons that append below search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
 let APIKey = "9d7113f3af5d25571785a917bd91f773";
+let city;
+let searchFormEl = document.querySelector('#search-input');
+let cityInputEl = document.querySelector('#city-input');
+let citiesListEl = document.querySelector('.cities');
+let citiesArr = [];
+
+
+
+searchFormEl.addEventListener('submit', formSubmitCity)
+
+function formSubmitCity (event) {
+    event.preventDefault();
+     
+    let searchCity = cityInputEl.value.trim();
+    
+    console.log(searchCity);
+   
+   if (searchCity) {cityInputEl.value = "";
+    localStorage.setItem("savedCity", searchCity);
+    let newCity = localStorage.getItem("savedCity");
+    console.log(newCity);
+   
+    let cityButton = document.createElement("button");
+    let cityBtnText = document.createTextNode(newCity);
+    cityButton.appendChild(cityBtnText);
+    
+    citiesListEl.appendChild(cityButton);}
+    //cityButton.setAttribute("name", "savedCity")
+
+   
+
+  
+  
+   /* if (username) {
+      getUserRepos(username);
+  
+      repoContainerEl.textContent = '';
+      nameInputEl.value = '';
+    } else {
+      alert('Please enter a GitHub username');
+    }*/
+  };
+
