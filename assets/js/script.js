8f8ade4e81ac43a731559dccc9161add001d22ae
Sarah Richens <sarahgrichens@gmail.com>
2023-01-20 15:21:14 -0600

    start working on pulling out city info from storage to render with populated buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,38 +13,38 @@ $('#today-date').text('(' + today.format('MMM D, YYYY') + ')');
 
 let d0 = new Date(today);
 d0.setDate(d0.getDate() + 1);
-console.log(d0);
+//console.log(d0);
 let dateFormat0 = new Date(d0);
 let day0 = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric'}).format(dateFormat0);
-console.log(day0);
+//console.log(day0);
 
 let d1 = new Date(today);
 d1.setDate(d1.getDate() + 2);
-console.log(d1);
+//console.log(d1);
 let dateFormat1 = new Date(d1);
 let day1 = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric'}).format(dateFormat1);
-console.log(day1);
+//console.log(day1);
 
 let d2 = new Date(today);
 d2.setDate(d2.getDate() + 3);
-console.log(d2);
+//console.log(d2);
 let dateFormat2 = new Date(d2);
 let day2 = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric'}).format(dateFormat2);
-console.log(day2);
+//console.log(day2);
 
 let d3 = new Date(today);
 d3.setDate(d3.getDate() + 4);
-console.log(d3);
+//console.log(d3);
 let dateFormat3 = new Date(d3);
 let day3 = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric'}).format(dateFormat3);
-console.log(day3);
+//console.log(day3);
 
 let d4 = new Date(today);
 d4.setDate(d4.getDate() + 5);
-console.log(d4);
+//console.log(d4);
 let dateFormat4 = new Date(d4);
 let day4 = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric'}).format(dateFormat4);
-console.log(day4);
+//console.log(day4);
 
 let nextDayArr = [day0, day1, day2, day3, day4];
 
@@ -52,7 +52,7 @@ for (i = 0; i < nextDayArr.length; i++) {
   document.getElementById('day' + [i] + 'D').innerHTML = nextDayArr[i];  
 }
 
-console.log(nextDayArr);
+//console.log(nextDayArr);
 
 
 
@@ -69,15 +69,30 @@ function formSubmitCity (event) {
     console.log(searchCity);
    
    if (searchCity) {cityInputEl.value = "";
-    localStorage.setItem("savedCity", searchCity);
-    let newCity = localStorage.getItem("savedCity");
+
+    let savedCity = {
+      city: searchCity,
+      id: searchCity
+    };
+
+    localStorage.setItem("savedCity", JSON.stringify(savedCity));
+    let newCity = JSON.parse(localStorage.getItem("savedCity"));
     console.log(newCity);
+
+    citiesArr.push(newCity);
+    console.log(citiesArr);
+    localStorage.setItem("allCitiesSearch", citiesArr);
    
     let cityButton = document.createElement("button");
-    let cityBtnText = document.createTextNode(newCity);
-    cityButton.appendChild(cityBtnText);
+    cityButton.setAttribute("id", newCity.id);
+    let cityBtnText = document.createTextNode(newCity.city);
+    cityButton.appendChild(cityBtnText); 
+    
+    
+    
     
     citiesListEl.appendChild(cityButton);}
+
 
     let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + APIKey;
 
@@ -85,14 +100,14 @@ function formSubmitCity (event) {
 
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
+        //console.log(response);
         response.json().then(function (data) {
-          console.log(data);
-          console.log(data.main.temp);
-          console.log(data.wind.speed);
-          console.log(data.main.humidity);
+          //console.log(data);
+          //console.log(data.main.temp);
+          //console.log(data.wind.speed);
+          //console.log(data.main.humidity);
           fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
-          console.log(fahrenheit);
+          //console.log(fahrenheit);
           document.getElementById('city-select').innerHTML = data.name;
           document.getElementById('temp-select').innerHTML = "Temp: " + fahrenheit + '\u00B0' + ' F';
           document.getElementById('wind-select').innerHTML = "Wind: " + data.wind.speed + ' mph';
@@ -112,9 +127,9 @@ function formSubmitCity (event) {
 
     .then(function (response) {
       if (response.ok) {
-        console.log(response);
+        //console.log(response);
         response.json().then(function (data) {
-          console.log(data);
+          //console.log(data);
           let dayOne = data.list[7];
           let dayTwo = data.list[15];
           let dayThree = data.list[23];
@@ -122,14 +137,9 @@ function formSubmitCity (event) {
           let dayFive = data.list[39];
           let dayArray = [dayOne, dayTwo, dayThree, dayFour, dayFive];
           
-
-          /*for (i = 0; i < dayArray.length; i++) {
-            document.getElementById('day' + [i] + 'D').innerHTML = dayArray[i].dt_txt;  
-          }*/
-
           for (i = 0; i < dayArray.length; i++) {  
             fahForecast = Math.round(((parseFloat(dayArray[i].main.temp)-273.15)*1.8)+32);
-            console.log(fahrenheit);         
+            //console.log(fahrenheit);         
             document.getElementById('day' + [i] + 'T').innerHTML = "Temp: " + fahForecast + '\u00B0' + ' F';         
           }
 
@@ -140,27 +150,12 @@ function formSubmitCity (event) {
           for (i = 0; i < dayArray.length; i++) {
             document.getElementById('day' + [i] + 'H').innerHTML = "Humidity: " + dayArray[i].main.humidity + '%';
           }
-
-
-          /*console.log(data.list[7].dt_txt);
-          console.log(data.list[7].main.temp);
-          console.log(data.list[7].wind.speed);
-          console.log(data.list[7].main.humidity);
-          document.getElementById('day1D').innerHTML = data.list[7].dt_txt;
-          document.getElementById('day1T').innerHTML = "Temp: " + data.list[7].main.temp;
-          document.getElementById('day1W').innerHTML = "Wind: " + data.list[7].wind.speed;
-          document.getElementById('day1H').innerHTML = "Humidity: " + data.list[7].main.humidity;*/
           
         });
       } else {
         alert('Error: ' + response.statusText);
       }
-    })
-
-   
-
-  
-  
-   
+    });
+     
   };
 
